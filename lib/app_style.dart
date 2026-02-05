47ecf00863c26f27dfe374b18d1970addc058581
madhu.pentakota <madhu.pentakota@excelglobalsolution.com>
2026-02-05 21:15:24 +0530

    app store

--- lib/app_style.dart
+++ lib/app_style.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color background = Color(0xFF121212); // Deep Black
-  static const Color surface = Color(0xFF1E1E1E);    // Dark Grey
-  static const Color primary = Color(0xFFC5A08E);    // Rose Gold/Copper
+  static const Color surface = Color(0xFF1E1E1E); // Dark Grey
+  static const Color primary = Color(0xFFC5A08E); // Rose Gold/Copper
   static const Color textMain = Colors.white;
   static const Color textSecondary = Colors.grey;
   static const Color inputBorder = Color(0xFF333333);
